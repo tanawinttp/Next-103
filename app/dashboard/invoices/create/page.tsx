874303eb6a03eae7d061ca372ff66ed7a5211c97
Tanawin(Toy) <tanawin@zimpligital.com>
2024-02-19 04:13:48 +0700

    create invoice page

---
+++ app/dashboard/invoices/create/page.tsx
@@ -0,0 +1,24 @@
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+
+const page = async () => {
+  const customer = await fetchCustomers();
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customer} />
+    </main>
+  );
+};
+
+export default page;
