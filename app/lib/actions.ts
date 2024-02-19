ba68cd3c4ff3fa16e2de2e14e69b8542677b581e
Tanawin(Toy) <tanawin@zimpligital.com>
2024-02-20 01:06:54 +0700

    finish chapter 12 add function CRUD invoice card

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -41,3 +41,29 @@ export async function createInvoice(formData: FormData) {
   //   console.log(rawFormData);
   //   console.log(typeof rawFormData.amount);
 }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export const updateInvoice = async (id: string, formData: FormData) => {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+    `;
+
+  revalidatePath(`/dashboard/invoices`);
+  redirect(`/dashboard/invoices`);
+};
+
+export const deleteInvoice = async (id: string) => {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath(`/dashboard/invoice`);
+};
